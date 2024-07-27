858373130c9fcb8bfdf525bd4670f22ebfafac8a
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-07-27 17:23:06 +0530

    steup done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <>
+        <div className="bg-dark">
+          <h1 className="text-success">Hello</h1>
+        </div>
+      </>
+    </div>
+  );
+}
+
+export default App;
