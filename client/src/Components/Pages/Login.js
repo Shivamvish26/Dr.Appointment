08ec2e86dbdced32de42e7a8a4ab639bb8ae6c1b
Shubham Vishwakarma <127945668+Shivamvish26@users.noreply.github.com>
2024-08-12 16:37:09 +0530

    line of code added

--- client/src/Components/Pages/Login.js
+++ client/src/Components/Pages/Login.js
@@ -1,9 +1,49 @@
 import React from "react";
+import "../Styles/RegisterStyle.css";
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+  const [form] = Form.useForm();
+
+  const onFinishHandler = (values) => {
+    console.log(values);
+    form.resetFields();
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="form-container">
+      <Form
+        form={form} // Connect form instance
+        layout="vertical"
+        onFinish={onFinishHandler}
+        className="register-form"
+      >
+        <h3 className="text-center">Login Form</h3>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: "Please input your email!" },
+            { type: "email", message: "The input is not valid E-mail!" },
+          ]}
+        >
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input type="password" />
+        </Form.Item>
+        <Link to="/register" className="text-decoration-none m-2">
+          Not a user? Register here
+        </Link>
+        <Button type="primary" htmlType="submit">
+          Login
+        </Button>
+      </Form>
     </div>
   );
 }
--- client/src/Components/Pages/Register.js
+++ client/src/Components/Pages/Register.js
@@ -1,73 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
 import "../Styles/RegisterStyle.css";
 import { Form, Input, Button } from "antd";
 import { Link } from "react-router-dom";
 
 export default function Register() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [form] = Form.useForm();
 
   const onFinishHandler = (values) => {
     console.log(values);
-    setName("");
-    setEmail("");
-    setPassword("");
+    form.resetFields();
   };
 
   return (
-    <>
-      <div className="form-container">
-        <Form
-          layout="vertical"
-          onFinish={onFinishHandler}
-          className="register-form"
+    <div className="form-container">
+      <Form
+        form={form} // Connect form instance
+        layout="vertical"
+        onFinish={onFinishHandler}
+        className="register-form"
+      >
+        <h3 className="text-center">Register Form</h3>
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[{ required: true, message: "Please input your name!" }]}
         >
-          <h3 className="text-center">Register Form</h3>
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[{ required: true, message: "Please input your name!" }]}
-          >
-            <Input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { required: true, message: "Please input your email!" },
-              { type: "email", message: "The input is not valid E-mail!" },
-            ]}
-          >
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Item>
-          <Link to="/login" className="text-decoration-none m-2">
-            Already user? Login here
-          </Link>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form>
-      </div>
-    </>
+          <Input type="text" />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { required: true, message: "Please input your email!" },
+            { type: "email", message: "The input is not valid E-mail!" },
+          ]}
+        >
+          <Input type="email" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input type="password" />
+        </Form.Item>
+        <Link to="/login" className="text-decoration-none m-2">
+          Already a user? Login here
+        </Link>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form>
+    </div>
   );
 }
